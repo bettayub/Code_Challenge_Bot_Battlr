fff9a222ca3f070f46a29a9f0df65c721dac9343
bettayub <bettayub19@gmail.com>
2023-07-30 13:11:57 +0300

    Implemented BotsPage component with state and API data fetching.

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,3 +1,16 @@
 import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
+
+function BotsPage() {
+  
+    const [bots, setBots] = useState([]);
+    const [botArmy, setBotArmy] = useState([]);
+    
+    useEffect(() => {
+      fetch("https://json-server2-vercel-nine.vercel.app/bots")
+      .then (res => res.json())
+      .then (bots => setBots(bots))
+    }, []);
+  
+  console.log(bots)
